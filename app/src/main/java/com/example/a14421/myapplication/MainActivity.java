422ecc868e4d8072233fcae6a2deb3e3c795105d
Sherry <1442153469@qq.com>
2016-10-13 17:09:59 +0800

    commit all

---
+++ app/src/main/java/com/example/a14421/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a14421.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.sherry);
+        textView.setText(getResources().getText(R.string.sherry));
+        textView.setTextColor(getResources().getColor(R.color.colorPrimary));
+    }
+}
